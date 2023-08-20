4c442b3a6b94dd0d6def369fedcbd4b4fdf67c1d
chiawen81 <fangchiawen@gmail.com>
2023-08-20 19:36:58 +0800

    Initial commit

---
+++ apps/search/src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'couples-trip-front-end-app-shell',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.css'],
+})
+export class AppShellComponent {}
