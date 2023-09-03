dddc4672b49d44f5829d632a21fdf2137059f29f
chiawen81 <fangchiawen@gmail.com>
2023-09-03 16:35:46 +0800

    部署測試- 解決報錯問題

--- start-app.js
+++ start-app.js
@@ -1,3 +1,7 @@
+console.log('啟動服務，已進入start-app.js');
+
+global.self = global; // 定義全域變數 self
+
 const fs = require('fs');
 const path = require('path');
 
@@ -16,9 +20,17 @@ fs.readdir(directoryPath, (err, files) => {
   if (mainFile) {
     const fullPath = path.join(directoryPath, mainFile);
 
-    // 使用 require 或其他方法執行 main 檔案
-    require(fullPath);
+    try {
+      // 使用 require 或其他方法執行 main 檔案
+      require(fullPath);
+      console.log('main.js 成功執行');
+
+    } catch (e) {
+      console.error('Error while requiring main file:', e);
+    };
+
   } else {
     console.log('main.js not found');
-  }
+  };
+
 });
